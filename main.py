3ad180516e9ec936419226ba58c99925aed85ca9
Moritz Waschkewitz <moritz.waschkewitz@stud.hn.de>
2025-08-12 13:50:04 +0200

    added functions to find empty courses, sort them by their last modification and adding human readable timestamps

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from MoodleCourseUserManager import MoodleCourseUserManager
 
-import json
+from datetime import datetime
 from pprint import pprint
 
 
+def find_empty_courses(course_users: dict[str, list], all_courses: dict[str, dict]) -> dict[str, dict]:
+    return {
+        course_id: all_courses[course_id]
+        for course_id, users in course_users.items()
+        if len(users) == 0 and course_id in all_courses
+    }
+
+
+# TODO: sort by last user access?
+def sort_courses_by_timemodified(courses: dict[str, dict]) -> list[dict]:
+    return sorted(courses.values(), key=lambda course: course['timemodified'])
+
+
+def add_readable_dates_to_courses(courses: list[dict]) -> None:
+    """ adds human-readable timestamps to list of courses """
+
+    for course in courses:
+        for key in ('timemodified', 'timecreated', 'startdate'):
+            value = course.get(key)
+            if isinstance(value, int):
+                course[f"{key}_human"] = datetime.fromtimestamp(course[key]).strftime('%Y-%m-%d %H:%M:%S')
+
+
 def main():
-    '''
-    courses = list_courses(ms.session, ms.settings.url, token=ms.token)
-    with open("courses.json", "w", encoding="utf-8") as courses_file:
-        json.dump(courses, courses_file, indent=2)
-
-    pe2_tut_users = list_course_users(ms.session, ms.settings.url, token=ms.token, course_id=PE2_TUT_ID)
-    save_course(PE2_TUT_ID, pe2_tut_users)
-    '''
-
     PE2_TUT_ID = '10979'
 
     manager = MoodleCourseUserManager(debug=True)
@@ -30,6 +44,15 @@ def main():
     pprint(pe2_tut_course)
 
     print(f"Eingeschriebene Nutzer/innen in PE2-TUT: {len(course_users[PE2_TUT_ID])}")
+
+    empty_courses = find_empty_courses(course_users, all_courses)
+    # pprint(empty_courses)
+
+    sorted_empty_courses = sort_courses_by_timemodified(empty_courses)
+    add_readable_dates_to_courses(sorted_empty_courses)
+    pprint(sorted_empty_courses[-1])
+
+    pprint(f"Anzahl leerer Kurse: {len(sorted_empty_courses)}")
 
 
 if __name__ == "__main__":
