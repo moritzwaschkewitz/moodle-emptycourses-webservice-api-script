0ded2084da900c7e7e483d9b4b2e8078d663396b
Moritz Waschkewitz <moritz.waschkewitz@stud.hn.de>
2025-08-26 15:05:12 +0200

    extracted __collect_empty_courses_by_supercategory into its own method

--- CourseAnalyzer.py
+++ CourseAnalyzer.py
@@ -106,15 +106,15 @@ class CourseAnalyzer:
 
         return all_course_users
 
-    def find_and_export_empty_courses_to_csv(self, excluded_supercatergory_ids=None, csv_directory_path: Path = Path.cwd()) -> None:
+    def find_and_export_empty_courses_to_csv(self, excluded_supercategory_ids=None, csv_directory_path: Path = Path.cwd()) -> None:
         """
 
-        :param excluded_supercatergory_ids: List of supercatergory IDs which should be excluded
+        :param excluded_supercategory_ids: List of supercatergory IDs which should be excluded
         :param csv_directory_path: Path to the directory where CSV files should be saved
         :return:
         """
-        if excluded_supercatergory_ids is None:
-            excluded_supercatergory_ids = []
+        if excluded_supercategory_ids is None:
+            excluded_supercategory_ids = []
 
         self.__logger.info(f"Fetching category_lookup...")
         category_lookup = self.__fetch_supercategory_lookup()
@@ -122,35 +122,7 @@ class CourseAnalyzer:
         self.__logger.info(f"Fetching courses_overview...")
         courses_overview = self.__fetch_courses_overview()
 
-        number_of_all_courses = len(courses_overview)
-        empty_courses = []
-
-        for i, (course_id, course_dict) in enumerate(courses_overview.items(), 1):
-            category_id = course_dict['categoryid']
-            name = course_dict['name']
-            if course_id == 1:
-                self.__logger.debug(f"Skipping {course_id}: {name}")
-                continue
-            supercategory_id = category_lookup[category_id]['supercategory']
-            if supercategory_id in excluded_supercatergory_ids:
-                continue
-
-            # TODO: find empty courses
-
-            # TODO: sort by last timestamp
-
-            # TODO: add_readable_dates_to_courses
-            # TODO: add_url_to_courses
-
-            '''Progress Bar'''
-            bar_length = 50
-            filled_length = int(bar_length * i / number_of_all_courses)
-            bar = '█' * filled_length + '-' * (bar_length - filled_length)
-            sys.stdout.write(f'\rProgress: |{bar}| {i}/{number_of_all_courses}')
-            sys.stdout.flush()
-
-        #print("\nFertig!")
-        self.__logger.info(f"Finished {number_of_all_courses} courses.")  # TODO better description
+        empty_courses_per_supercategory = self.__collect_empty_courses_by_supercategory(category_lookup, courses_overview, excluded_supercategory_ids)
 
         # TODO: export to csv
 
@@ -174,7 +146,7 @@ class CourseAnalyzer:
             return None
         return int(parts[1])
 
-    def __fetch_supercategory_lookup(self) -> dict[str, dict]:
+    def __fetch_supercategory_lookup(self) -> dict[int, dict]:
         """
         Build a lookup dictionary of all Moodle categories with their names
         and supercategory IDs.
@@ -232,7 +204,7 @@ class CourseAnalyzer:
         )
         courses_overview = {}
         for course in all_courses_list:
-            timestamp_keys_to_check = ["timecreated", "timemodified", "startdate"]
+            timestamp_keys_to_check = ['timecreated', 'timemodified', 'startdate']
             filtered_timestamp_dict = {k: course[k] for k in timestamp_keys_to_check}
             latest_category, latest_timestamp = max(filtered_timestamp_dict.items(), key=lambda x: x[1])
             courses_overview[course['id']] = {
@@ -245,3 +217,80 @@ class CourseAnalyzer:
             }
 
         return courses_overview
+
+    def __collect_empty_courses_by_supercategory(self,
+                                                 category_lookup: dict[int, dict],
+                                                 courses_overview: dict[int, dict],
+                                                 excluded_supercategory_ids: list[int]) \
+            -> dict[str, list[dict]]:
+        """
+            Collects all courses with no enrolled users, grouped by their faculty/supercategory.
+
+            Iterates through the provided course overview, skips excluded courses or categories,
+            queries the user list for each course, and builds a dictionary of empty courses organized
+            by faculty. Progress is printed to stdout during execution.
+
+            Each faculty name provides the string key for the outer dictionary with a list of empty courses as the value.
+            The list of empty courses gets sorted by the last timestamp and contains the following course-metadate in a dictionary:
+
+            - **id** (int): course-ID.
+            - **fullname** (str): Full name of the course.
+            - **shortname** (str): Short name of the course.
+            - **categoryid** (int): ID of the associated category.
+            - **latest_key** (str): Key of the most recent timestamp.
+            - **latest_timestamp_unix** (int): Most recent timestamp in Unix time.
+            - **latest_timestamp_human** (str): Most recent timestamp in YYYY-MM-DD HH:MM:SS.
+            - **category** (str): name of the direct parent category.
+            - **url** (str): moodle-URL.
+
+            :returns: Dictionary mapping faculty names to a list of empty courses.
+            :param category_lookup: result of self.__fetch_supercategory_lookup.
+            :param courses_overview: result of self.__fetch_courses_overview.
+            :param excluded_supercategory_ids: list of supercategory IDs to exclude.
+        """
+        number_of_all_courses = len(courses_overview)
+        empty_courses_per_supercategory = {}
+
+        for i, (course_id, course_dict) in enumerate(courses_overview.items(), 1):
+            category_id = course_dict['categoryid']
+            name = course_dict['fullname']
+            if course_id == 1:
+                self.__logger.debug(f"Skipping {course_id}: {name}")
+                continue
+            supercategory_id = category_lookup[category_id]['supercategory']
+            if supercategory_id in excluded_supercategory_ids:
+                continue
+
+            course_users = list_course_users(
+                session=self.__moodle_session.session,
+                base_url=self.__moodle_session.settings.url,
+                token=self.__moodle_session.token,
+                course_id=course_id,
+            )
+            if len(course_users) == 0:
+                supercategory_name = category_lookup[category_id]['name']
+
+                if supercategory_name not in empty_courses_per_supercategory:
+                    empty_courses_per_supercategory[supercategory_name] = []
+
+                empty_courses_per_supercategory[supercategory_name].append({
+                    'id': course_id,
+                    'category': category_lookup[category_id]['name'],
+                    'url': f"https://moodle.hsnr.de/course/view.php?id={course_id}",
+                    **course_dict
+                })
+
+            '''Progress Bar'''
+            bar_length = 50
+            filled_length = int(bar_length * i / number_of_all_courses)
+            bar = '█' * filled_length + '-' * (bar_length - filled_length)
+            sys.stdout.write(f"\rProgress: |{bar}| {i}/{number_of_all_courses}"
+                             f"\tFound empty courses: {len(empty_courses_per_supercategory)}")
+            sys.stdout.flush()
+
+        self.__logger.info(f"Finished {number_of_all_courses} courses.")  # TODO better description
+
+        for empty_courses_list in empty_courses_per_supercategory.values():
+            empty_courses_list.sort(key=lambda x: x['latest_timestamp_unix'])
+
+        return empty_courses_per_supercategory
