c406ae518d8e8a2cc4a34a8fe7efa4360fb96f8e
Moritz Waschkewitz <moritz.waschkewitz@stud.hn.de>
2025-11-17 13:27:02 +0100

    added timestamps for main-usage

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 from datetime import datetime
 
-from CourseAnalyzer import CourseAnalyzer
+from course_analyzer import CourseAnalyzer
 
 
 def main():
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     manager = CourseAnalyzer()
-    manager.find_and_export_empty_courses_to_csv([], Path.cwd() / 'csv')
+    manager.find_and_export_empty_courses_to_csv([], Path.cwd() / 'csv_exports')
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 
 if __name__ == "__main__":
